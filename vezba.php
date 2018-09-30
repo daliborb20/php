d496b518234f99c1e3439f44619617efc8247fbf
daliborb20@gmail.com <daliborb20@gmail.com>
2018-09-30 19:56:42 +0200

    prvo dodavanje

---
+++ vezba.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>Hallo motherfucker</h1>";
+$varijabla = [1, 2, 3, 4, 5, 6, 1, 11, 111, 1, 1];
+for($i=0; $i < count($varijabla);$i++){
+     echo"<h1>".$varijabla[$i]."</h1>";
+}
+echo($varijabla)
+
+?>
